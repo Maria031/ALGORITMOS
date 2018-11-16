c14297b4f12f03b971360296592ed8eb8c6ac883
Maria <43083916+Maria031@users.noreply.github.com>
2018-11-15 18:39:17 -0600

    Add files via upload

---
+++ Unidad 4 act 3.R
@@ -0,0 +1,12 @@
+Datos <- read.csv("C://Users//madey//Documents//Algoritmos//Peso y Altura.csv")
+{
+X= Datos$PesoTotal = colSums(Datos[2])
+Y= Datos$AlturaTotal = colSums(Datos[3])
+
+print(X)
+print(Y)  
+
+print(X + Y)
+}
+
+
